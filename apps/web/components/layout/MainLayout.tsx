1e84488c1f533499ec1c29a5ed0eed5e572ed58d
REV_PRO <rev_pro@REV-PROui-MacBookPro.local>
2026-01-25 20:44:43 +0900

    fix: 우측 영역 패딩 50/100

--- apps/web/components/layout/MainLayout.tsx
+++ apps/web/components/layout/MainLayout.tsx
@@ -35,7 +35,7 @@ export default function MainLayout({ pwaContent, extendedContent, activeMenu = '
           {/* 우측 확장 콘텐츠 영역 - 768px 이상에서만 보임, 850px 고정 */}
           <div className="hidden md:block w-[850px] flex-shrink-0 h-screen overflow-y-auto">
             {/* 콘텐츠 - 패딩만 */}
-            <div className="px-[20px] pt-[20px] pb-[20px]">
+            <div className="px-[50px] pt-[100px] pb-[50px]">
               {extendedContent}
             </div>
           </div>
